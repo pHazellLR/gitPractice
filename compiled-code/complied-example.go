36b19724455f03631dbd1a8e8b70495b4188a941
paul hazell <paul.hazell@landregistry.gov.uk>
2020-10-12 14:56:33 +0100

    fix complied code

--- compiled-code/complied-example.go
+++ compiled-code/complied-example.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("I am a compiled computer programme\nI am written in Go but you don't have Go installed on this machine.\nInstead Go programmes are complied (built) into binary executables that target a specifc operating system and processor architecture")
+	fmt.Println("This programme will self-destruct after 30 seconds")
+	time.Sleep(30000 * time.Millisecond)
 }
